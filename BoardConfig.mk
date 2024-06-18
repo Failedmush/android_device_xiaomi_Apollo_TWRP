eaf9530ab284ddc5c5d110738f3f4dab1c9e2071
sekaiacg <sekaiacg@gmail.com>
2024-06-18 19:22:29 +0800

    BoardConfig: Exclude apatch/kernelsu path
    Signed-off-by: sekaiacg <sekaiacg@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -177,5 +177,5 @@ TARGET_USES_LOGD := true
 TW_NO_SCREEN_BLANK := true
 TW_HAS_EDL_MODE := false
 TW_SUPPORT_INPUT_AIDL_HAPTICS :=true
-TW_BACKUP_EXCLUSIONS := /data/fonts
+TW_BACKUP_EXCLUSIONS := /data/fonts,/data/adb/ap,/data/adb/ksu
 TW_FRAMERATE := 60
