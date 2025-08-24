2c4fcb9af5fb2826755f6f8b46cda62edd45d535
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-08-24 22:35:18 +0900

    Update twrp_apollo.mk

--- twrp_apollo.mk
+++ twrp_apollo.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 
-DEVICE_PATH := device/xiaomi/umi
+DEVICE_PATH := device/xiaomi/apollo
 
 # Inherit from device.mk configuration
 $(call inherit-product, $(DEVICE_PATH)/device.mk)
